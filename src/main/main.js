8238c9f498f2fc25fd48f369073b2e13e7e1ef9d
seufster <seufster@gmail.com>
2018-02-25 15:43:29 -0500

    version update

--- src/main/main.js
+++ src/main/main.js
@@ -67,7 +67,7 @@ function createWindow () {
   const template = [{
       label: "SafeLedger",
       submenu: [
-          { label: "Version 1.5"},
+          { label: "Version 1.6"},
           { type: "separator" },
           { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
       ]}, {
