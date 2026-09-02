cff656024dfe175bee2b73b4313aa89fe9bb6fcf
MieleMadness <84972636+MieleMadness@users.noreply.github.com>
2026-09-02 18:59:35 -0400

    Load Vault Item selection guard

--- src/main/renderer-entry.js
+++ src/main/renderer-entry.js
@@ -3,6 +3,7 @@
 require('./app-appearance.js');
 require('./startup-ui.js');
 require('./renderer.js');
+require('./vault-item-selection-ui.js');
 require('./privacy-mode-ui.js');
 require('./dashboard-ui.js');
 require('./dashboard-row-ui.js');
